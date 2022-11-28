23487648b84736805c9693fdf789368ac0b3efcf
E526729 <ahmed.zekri.a@gmail.com>
2022-11-29 00:53:20 +0100

    error handling add habit

--- app/src/main/java/com/example/habittracker/domain/use_case/AddHabitUseCase.kt
+++ app/src/main/java/com/example/habittracker/domain/use_case/AddHabitUseCase.kt
@@ -7,11 +7,15 @@ import kotlinx.coroutines.flow.flow
 
 class AddHabitUseCase constructor(private val habitsRepository: HabitsRepository) {
     operator fun invoke(habit: Habit) = flow {
-        if (habitsRepository.getHabitsByName(habit.name).isNotEmpty())
-            emit(ResultHolder.Error(error = "Habit with the same name already exists"))
-        else {
-            habitsRepository.addHabit(habit)
-            emit(ResultHolder.Success(data = habit))
+        try {
+            if (habitsRepository.getHabitsByName(habit.name).isNotEmpty())
+                emit(ResultHolder.Error(error = "Habit with the same name already exists"))
+            else {
+                habitsRepository.addHabit(habit)
+                emit(ResultHolder.Success(data = habit))
+            }
+        } catch (e: Exception) {
+            emit(ResultHolder.Error(error = e.message ?: ""))
         }
     }
 }
