b6ca4994d517db5ecba829279acace3ccc0d4135
E526729 <ahmed.zekri.a@gmail.com>
2022-12-04 02:22:58 +0100

    Adding icons

---
+++ app/src/main/java/com/example/habittracker/data/constants/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.habittracker.data.constants
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
+val ICONS = listOf(
+    Icons.Filled.Image,
+    Icons.Filled.Abc,
+    Icons.Filled.ArrowForward,
+    Icons.Filled.Add,
+    Icons.Filled.VideoFile,
+    Icons.Filled.AcUnit,
+    )
--- app/src/main/java/com/example/habittracker/ui/mainScreen/HabitsScreen.kt
+++ app/src/main/java/com/example/habittracker/ui/mainScreen/HabitsScreen.kt
@@ -1,25 +1,102 @@
 package com.example.habittracker.ui.mainScreen
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Accessibility
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.habittracker.data.constants.ICONS
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HabitsScreen(mainScreenViewModel: HabitViewModel = viewModel()) {
     val state by mainScreenViewModel.state
-    if (state.error.isNotEmpty())
-        Text(text = state.error)
-    else
-        LazyVerticalGrid(columns = GridCells.Adaptive(160.dp), content = {
-            items(state.habits.size) { index ->
-                Card {
-                    Text(text = state.habits[index].name)
+    val openDialog = remember { mutableStateOf(false) }
+    val openImageDialog = remember { mutableStateOf(false) }
+
+    LazyVerticalGrid(modifier = Modifier
+        .background(color = Color(255, 112, 76))
+        .fillMaxSize(), columns = GridCells.Adaptive(160.dp), content = {
+        items(state.habits.size + 1) { index ->
+            Box(
+                modifier = Modifier
+                    .size(150.dp)
+                    .padding(5.dp)
+            ) {
+                Canvas(modifier = Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        if (index < state.habits.size) showHabit() else openDialog.value = true
+                    }) {
+                    drawCircle(color = Color.White, style = Stroke(25f), radius = 150f)
                 }
+
+                Icon(
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(70.dp),
+                    imageVector = if (index < state.habits.size) Icons.Default.Accessibility else Icons.Default.Add,
+                    contentDescription = null
+                )
             }
-        })
+        }
+    })
+    if (openDialog.value)
+        AlertDialog(
+            onDismissRequest = { },
+            confirmButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                }) { Text(text = "Add habit") }
+            },
+            title = { Text(text = "Adding Habit") },
+            text = {
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    placeholder = { Text(text = "Enter habit name") })
+                Button(onClick = {openImageDialog.value=true}) {
+                    
+                }
+            })
+    if (openImageDialog.value)
+        AlertDialog(
+            onDismissRequest = { },
+            confirmButton = {
+                Button(onClick = {
+                    openDialog.value = false
+                }) { Text(text = "Choose an image") }
+            },
+            title = { Text(text = "Choosing image") },
+            text = {
+                LazyVerticalGrid(columns = GridCells.Adaptive(80.dp)) {
+                    items(ICONS.size){
+                        index->
+                        Icon(imageVector = ICONS[index], contentDescription =null )
+
+                    }
+                }
+            })
+}
+
+fun showHabit() {
+    TODO("Not yet implemented")
 }
